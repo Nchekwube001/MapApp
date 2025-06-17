3b7f2c4a236e155a6f1a9b452f7b5a330710ceda
Unekwe Francis <unekwenchekwube@gmail.com>
2025-06-17 09:29:40 +0100

    Setup folders and services

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by francis on 17/06/2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+   let  name: String;
+   let  cityName: String;
+    let  coordinates: CLLocationCoordinate2D;
+   let  description: String;
+    let  imageNames: [String];
+   let  link: String;
+ 
+   
+}
