eb5f45d5a09e52266baf4c5f4904080c3ddd0cfe
Unekwe Francis <unekwenchekwube@gmail.com>
2025-06-17 08:37:10 +0100

    Initial Commit

---
+++ MapAppTests/MapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppTests.swift
+//  MapAppTests
+//
+//  Created by francis on 17/06/2025.
+//
+
+import Testing
+@testable import MapApp
+
+struct MapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
